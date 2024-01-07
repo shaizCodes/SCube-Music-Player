6911d7600a0d788a7cae58e684b60f62e70b266c
sidraRashdi <rashdimuntaha557@gmail.com>
2024-01-07 20:32:26 +0500

    added song description activity

---
+++ app/src/main/java/edu/sibau/scube_music_player/PlayPause.kt
@@ -0,0 +1,11 @@
+package edu.sibau.scube_music_player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayPause : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_pause)
+    }
+}
